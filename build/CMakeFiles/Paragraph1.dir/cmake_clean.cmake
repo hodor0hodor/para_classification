486558c3226ab810a55f3006a3018ff9143495da
riddhishb <riddhishb@gmail.com>
2014-08-22 14:21:28 +0530

    The_Basic_Folder

---
+++ build/CMakeFiles/Paragraph1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Paragraph1.dir/Paragraph1.cpp.o"
+  "Paragraph1.pdb"
+  "Paragraph1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Paragraph1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
